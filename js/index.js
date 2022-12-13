16658461be40bc2bc4e74d0b4b5a7bc8ba222f6c
Davi Ferreira <daviferreiraaew@gmail.com>
2022-12-13 15:38:00 -0300

    POO JavaScript

--- js/index.js
+++ js/index.js
@@ -1,31 +1,14 @@
-const routes = {
-  '/': '/pages/home.html',
-  '/about': '/pages/about.html',
-  '/contact': '/pages/contact.html',
-  404: '/pages/404.html'
-}
+import './router.js'
 
-function route(event) {
-  event = event || window.event
-  event.preventDefault()
+// const routes = {
+//   '/': '/pages/home.html',
+//   '/about': '/pages/about.html',
+//   '/contact': '/pages/contact.html',
+//   404: '/pages/404.html'
+// }
 
-  window.history.pushState({}, '', event.target.href)
 
-  handle()
-}
+// handle()
 
-function handle() {
-  const { pathname } = window.location
-  const route = routes[pathname] || routes[404]
-
-  fetch(route)
-    .then(data => data.text())
-    .then(html => {
-      document.querySelector('#app').innerHTML = html
-    })
-}
-
-handle()
-
-window.onpopstate = () => handle()
-window.route = () => route()
+// window.onpopstate = () => handle()
+// window.route = () => route()
